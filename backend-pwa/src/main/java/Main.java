f7767766008176210bf2c3b19d0b926a87ea772f
yanis92110 <yanislaidd@gmail.com>
2025-11-01 17:01:09 +0100

    http server

--- backend-pwa/src/main/java/Main.java
+++ backend-pwa/src/main/java/Main.java
@@ -1,11 +1,27 @@
 import serveur.ServeurWebSocket;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Main {
     @SuppressWarnings("CallToPrintStackTrace")
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String portEnv = System.getenv("PORT");
         int port = (portEnv != null) ? Integer.parseInt(portEnv) : 8080;
         int nbJoueurs = 10; // Nombre de joueurs max
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+        httpServer.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Serveur WebSocket actif !";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                exchange.getResponseBody().write(response.getBytes());
+                exchange.close();
+            }
+        });
+        httpServer.start();
         
         System.out.println("===========================================");
         System.out.println("  Aisselle Emeute Caca - Serveur PWA");
